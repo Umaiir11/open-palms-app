4b010a3abbb38169efa8e3862db190514b44e3f0
arslan_riaz_jeux <riaz008991@gmail.com>
2025-09-01 14:25:45 +0500

    Push By Arslan on 1st September Working on Frontend Design

--- lib/app/config/app_assets.dart
+++ lib/app/config/app_assets.dart
@@ -10,11 +10,12 @@ class AppAssets {
   // Images Path
   static const String splashLogo = '$imagesPath/splash-logo.png';
   static const String getStartedBg = '$imagesPath/get_started_bg.png';
-
+  static const String donateIcon = '$imagesPath/donate_icon.png';
+  static const String profileIcon = '$imagesPath/profile_icon.png';
+  static const String historyIcon = '$imagesPath/history_icon.png';
 
   // Svgs Path
   static const String whiteBackButton = '$svgsPath/white_back_button.svg';
   static const String blackBackButton = '$svgsPath/black_back_button.svg';
   static const String openPalmsLogo = '$svgsPath/open_palms_logo.svg';
-
 }
---
+++ lib/app/mvvm/view/donor_side/donor_bottom_bar_view/donor_bottom_bar_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DonorBottomBarView extends StatefulWidget {
+  const DonorBottomBarView({super.key});
+
+  @override
+  State<DonorBottomBarView> createState() => _DonorBottomBarViewState();
+}
+
+class _DonorBottomBarViewState extends State<DonorBottomBarView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
